82790877af27ffe1055e090b239c53d0f0ccdd21
Lev Mitnitskiy <levm@jfrog.com>
2021-12-23 17:21:33 +0200

    EventBusFactory added and init method changed.

--- src/main/java/com/demo/restservice/controller/SubscriptionController.java
+++ src/main/java/com/demo/restservice/controller/SubscriptionController.java
@@ -4,7 +4,6 @@ import com.demo.restservice.events.EventBusFactory;
 import com.demo.restservice.events.subscription.SubscriptionCreateEvent;
 import com.demo.restservice.events.subscription.SubscriptionUpdateEvent;
 import com.google.common.eventbus.EventBus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
